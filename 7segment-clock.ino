7ab7fcdfd20c61721f9184043809436f299a24a7
DaniFoldi <dani@stefan.hu>
2017-12-03 17:59:47 +0100

    Libraries, base code

---
+++ 7segment-clock.ino
@@ -0,0 +1,20 @@
+#include <SevSeg.h>
+#include <Wire.h>
+#include <DS3231.h>
+
+
+/*
+ * LIBRARIES NEEDED:
+ * 
+ * https://github.com/jarzebski/Arduino-DS3231
+ * https://github.com/DeanIsMe/SevSeg
+ * 
+ */
+
+void setup() {
+
+}
+
+void loop() {
+
+}
