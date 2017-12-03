11c1a9e9b328540af9756fec113b497980b49564
DaniFoldi <dani@stefan.hu>
2017-12-03 18:00:25 +0100

    Added constants and debug mode

--- 7segment-clock.ino
+++ 7segment-clock.ino
@@ -2,6 +2,23 @@
 #include <Wire.h>
 #include <DS3231.h>
 
+#define DEBUG 0
+
+const int button1_pin = 4;
+const int button2_pin = 3;
+
+const int segA = 8;
+const int segB = 9;
+const int segC = 10;
+const int segD = 11;
+const int segE = 12;
+const int segF = 13;
+const int segG = 7;
+
+const int anode1 = A3;
+const int anode2 = A2;
+const int anode3 = A1;
+const int anode4 = A0;
 
 /*
  * LIBRARIES NEEDED:
@@ -12,6 +29,10 @@
  */
 
 void setup() {
+  if (DEBUG) {
+    Serial.begin(9600);
+    while (!Serial) {}
+  }
 
 }
 
